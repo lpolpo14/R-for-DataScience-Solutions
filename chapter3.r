180efc191b887225d315fa3356f223003ed5c655
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2025-03-22 22:22:28 +0200

    nearly finished chapter 3

--- chapter3.r
+++ chapter3.r
@@ -90,3 +90,66 @@ filter(flights, min_rank(desc(dep_delay)) <= 10) # We have got no ties in this e
 
 # Exercise 6 Using ?sin we can see that it provides us with
 #cos(x) sin(x) tan(x) acos(x) asin(x) atan(x) atan2(y, x) cospi(x) sinpi(x) and tanpi(x).
+
+# Exercise 1 - We can try the median, the variance, the percentage of on time arrivals,
+# the percentage of on time arrivals and departures, and the percentage of on time departures.
+# Most important for us is the arrival time.
+
+# Exercise 2 
+
+not_cancelled <- flights %>% filter(!is.na(dep_delay), !is.na(arr_delay))
+#(not_cancelled %>% group_by(dest) %>% summarize(n())) The Same
+(not_cancelled %>% group_by(tailnum) %>% summarize(sum(distance)))
+#(not_cancelled %>% count(tailnum, wt = distance)) The Same
+
+# Exercise 3 : is.na(arr_delay) is enough. (Some flights may never reach the destination sadly..)
+
+# Exercise 4 : I will leave it for another day. (!!!)
+
+# Exercise 5
+mean_delay_by_carrier <- flights %>% group_by(carrier) %>% 
+  summarize(mean_delay = mean(arr_delay, na.rm = TRUE))
+mean_delay_by_carrier %>% arrange(desc(mean_delay)) # Result is F9 AirTran Airways
+# Depends. I will only check for F9 and all the airports it departs from.
+# If the mean is similar for every airport, then we can say that F9 has an excuse.
+flights %>% filter(carrier == "F9") %>% group_by(origin) %>% summarize(n())
+# We can see the specific carrier supports only one flight LGA to DEN. Let's see the mean for the airport.
+(mean_F9_depart <- flights %>% filter(origin == "LGA") %>% group_by(origin) %>%
+  summarize(mean(dep_delay, na.rm = TRUE)))
+(mean_F9_arrive <- flights %>% filter(origin == "LGA") %>% group_by(origin) %>%
+    summarize(mean(arr_delay, na.rm = TRUE)))
+
+# We can see that the carrier's delay is much more often than the airports.
+
+# Exercise 6 (The solution was found online and it is pretty neat.)
+# What personally helps me is employing a logic similar to that of sql queries, give it a try!
+# link is : https://jrnold.github.io/r4ds-exercise-solutions/workflow-basics.html
+# (Truth be told, I got extremely dissapointed such a great site with solutions already exists..
+# ruins the joy of solving in a way. Oh well, it is what it is.)
+flights %>%
+  # sort in increasing order
+  select(tailnum, year, month,day, dep_delay) %>%
+  filter(!is.na(dep_delay)) %>%
+  arrange(tailnum, year, month, day) %>%
+  group_by(tailnum) %>%
+  # cumulative number of flights delayed over one hour
+  mutate(cumulative_hr_delays = cumsum(dep_delay > 60)) %>%
+  # count the number of flights == 0
+  summarise(total_flights = sum(cumulative_hr_delays < 1)) %>%
+  arrange(total_flights)
+
+# Exercise 7 - If TRUE, it will show the largest groups at the top.
+# We could use it instead of arrange with desc().
+
+# Exercise 1 - a Window function returns n values when given n inputs, and include variations on
+# aggregate functions, functions for ranking and ordering, and functions for taking offsets (lag())
+# The given command vignette("window-functions") contains a lot of examples.
+
+# Exercise 2 - Once more, I will use the provided solution by the same site due to its efficiency.
+flights %>%
+  filter(!is.na(tailnum)) %>%
+  mutate(on_time = !is.na(arr_time) & (arr_delay <= 0)) %>%
+  group_by(tailnum) %>%
+  summarise(on_time = mean(on_time), n = n()) %>%
+  filter(min_rank(on_time) == 1) # The interesting idea!
+
